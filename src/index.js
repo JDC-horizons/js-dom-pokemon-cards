3b78f8730cafad20893d8cd666b38b430fc494fc
James Canterbury <jamesdcanterbury@protonmail.com>
2024-05-31 15:50:47 +0100

    core complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log(data);
+// console.log(data);
 
 //You can start simple and just render a single
 //pokemon card from the first element
@@ -8,35 +8,37 @@ console.log(data);
 //   let pokemon = data[i];
 // }
 
-const pokemonData = data[0];
+for (let i = 0; i < data.length; i++) {
+  const pokemonData = data[i];
 
-let newCard = document.createElement("li"); //makes a card
+  let newCard = document.createElement("li"); //makes a card
+  newCard.classList.add("card");
 
-const pokeName = document.createElement("h2"); //makes a name heading
-pokeName.textContent = pokemonData.name; //populates name info
-newCard.appendChild(pokeName);
+  const pokeName = document.createElement("h2"); //create h2 element
+  pokeName.textContent =
+    pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1); //populates name info
+  newCard.appendChild(pokeName);
 
-const pokeImage = document.createElement("img");
-pokeImage.setAttribute("width", "256");
-pokeImage.setAttribute("class", "card--img");
-pokeImage.setAttribute(
-  "src",
-  pokemonData.sprites.other["official-artwork"].front_default
-);
-newCard.appendChild(pokeImage);
+  const pokeImage = document.createElement("img"); //create img element
+  pokeImage.setAttribute("width", "256");
+  pokeImage.setAttribute("class", "card--img");
+  pokeImage.setAttribute(
+    "src",
+    pokemonData.sprites.other["official-artwork"].front_default //populates img src
+  );
+  newCard.appendChild(pokeImage);
 
-const pokeStats = document.createElement("ul"); //create initial list for stats
-newCard.appendChild(pokeStats);
+  const pokeStats = document.createElement("ul"); //create initial list for stats
+  for (let i = 0; i < pokemonData.stats.length; i++) {
+    //loop to populate each stat with info
+    let tempStat = pokemonData.stats[i];
+    let statElement = document.createElement("li");
+    const statName = tempStat.stat.name;
+    const statValue = tempStat.base_stat;
+    statElement.textContent = `${statName}: ${statValue}`.toUpperCase(); //populates stat info
+    pokeStats.appendChild(statElement);
+  }
+  newCard.appendChild(pokeStats); //appends filled list to card
 
-for (let i = 0; i > pokemonData.stats.length; i++) {
-  //loop to populate each stat with info
-  let tempStat = pokemonData.stats[i];
-  let statElement = document.createElement("li");
-  const statName = tempStat.stat.name;
-  const statValue = tempStat.base_stat;
-  statElement.textContent = `${statName}: ${statValue}`;
-
-  pokeStats.appendChild(statElement);
+  document.querySelector(".cards").appendChild(newCard); //appends finished card to page
 }
-
-document.querySelector(".cards").appendChild(newCard);
